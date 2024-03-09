70a6db1fb9a11c16799112b9a0993140eade2f0f
VanChien411 <phuthienchien3@gmail.com>
2024-03-09 09:57:05 +0700

    j

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from gradio_client import Client
+import os
 
 app = Flask(__name__)
 client = Client("ShynBui/Vector_db")
@@ -7,17 +8,13 @@ client = Client("ShynBui/Vector_db")
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.json
-    # client = Client("ShynBui/Vector_db")
-    # result = client.predict(
-	# 	data,	# str  in 'quote' Textbox component
-	# 	api_name="/predict"
-    # )
     result = data 
     if result:
-
         return jsonify(result), 200
     else:
         return jsonify({"error": "Failed to connect to database"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Use the PORT environment variable provided by Heroku
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, port=port, host='0.0.0.0')
