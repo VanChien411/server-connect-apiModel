73a467d9789ad84212636d44948c8fdd67e9ee87
VanChien411 <phuthienchien3@gmail.com>
2024-03-09 09:32:45 +0700

    first load

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from gradio_client import Client
+
+app = Flask(__name__)
+client = Client("ShynBui/Vector_db")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+    client = Client("ShynBui/Vector_db")
+    result = client.predict(
+		data,	# str  in 'quote' Textbox component
+		api_name="/predict"
+    )
+
+    if result:
+
+        return jsonify(result), 200
+    else:
+        return jsonify({"error": "Failed to connect to database"}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
