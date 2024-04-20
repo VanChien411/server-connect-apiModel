ed73ac2b576a18c855d1554c17523901b3cf896c
VanChien411 <phuthienchien3@gmail.com>
2024-04-20 23:19:53 +0700

    update model

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
-client = Client("ShynBui/Vector_db")
+client = Client("ShynBui/Vector_db_v2")
 
 @app.route("/predict", methods=["POST"])
 def predict():
